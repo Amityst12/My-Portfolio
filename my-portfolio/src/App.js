717f1be28bf4874127f480319bf3598bd10c3e09
Amit Yehoshaphat <amityst12@gmail.com>
2025-03-25 00:38:22 +0200

    starting.

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import {Bar} from "./Components/NavBar.js";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Bar />
+    </div>
+  );
+}
+
+export default App;
