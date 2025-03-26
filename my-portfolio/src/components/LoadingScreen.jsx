7b379fc228619d1c41e065f71d972e6f0886e971
Amit Yehoshaphat <amityst12@gmail.com>
2025-03-26 19:23:52 +0200

    New project
    Loading animation

---
+++ my-portfolio/src/components/LoadingScreen.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState  } from "react";
+
+export const LoadingScreen = ( {onComplete} ) => { //Make loading animation
+    const[text, setText] = useState("");
+    const fullText = "Amit's Portfolio";
+
+    useEffect(() =>{ //Set interval
+        let index = 0;
+        const interval = setInterval(() => {
+            setText(fullText.substring(0, index));
+            index++; // Increace letter
+
+            if(index> fullText.length){
+                clearInterval(interval);
+
+                setTimeout(()=> {
+                    onComplete();
+                },1000);
+            }
+        }, 100) //Time
+
+        return () => clearInterval(interval) //Case of death
+    }, [onComplete]);
+
+
+    return(
+     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+        <div className="mb-4 text-4xl font-mono font-bold">
+            {text} <span className="animate-blink ml-1"> | </span> 
+        </div>
+
+        <div className="w-[300px] h-[3px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+
+        </div>
+    </div>
+    );
+};
