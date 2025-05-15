79ea0363a7e9af08900d77e0ce8adacf8f459b54
Amit Yehoshaphat <amityst12@gmail.com>
2025-05-15 12:13:48 +0300

    Promptify

--- my-portfolio/src/components/sections/Porjects.jsx
+++ my-portfolio/src/components/sections/Porjects.jsx
@@ -99,6 +99,41 @@ export const Projects = () => {
                     </div>
                     </RevealOnScroll>
                 </div>
+
+
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all duration-300">
+                <RevealOnScroll>
+                    <h3 className="text-xl font-bold mb-2 group-hover:text-blue-400 transition-colors">
+                    💡 Promptify:
+                    </h3>
+                    <p className="text-gray-400 mb-4 break-words">
+                    A smart prompt enhancer web app that helps users turn simple or vague inputs into clear, structured, and detailed prompts for better AI results. Built with React and OpenAI API, Promptify features live prompt improvements, copy-to-clipboard, prompt history with local storage, and full responsiveness.
+                    </p>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                    {["React", "OpenAI", "TailwindCSS", "Vite", "JavaScript"].map((item, key) => (
+                        <span
+                        key={key}
+                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm
+                                    hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                        >
+                        {item}
+                        </span>
+                    ))}
+                    </div>
+                    <div className="flex justify-between items-center">
+                    <a
+                        href="https://www.linkedin.com/feed/update/urn:li:activity:7328675601815330819/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                    >
+                        View Project ↗
+                    </a>
+                    </div>
+                </RevealOnScroll>
+                </div>
+
             </div>
         </div>
 
